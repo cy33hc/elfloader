cbb31c90c429d1fd0bd787de05e5e6e159a43f5e
Chee Yee <cheeyee09@gmail.com>
2025-03-29 04:16:44 -0700

    updated elfldr url

--- src/main/java/org/ps5jb/client/payloads/ElfLoader.java
+++ src/main/java/org/ps5jb/client/payloads/ElfLoader.java
@@ -33,7 +33,7 @@ public class ElfLoader implements Runnable {
     private ProcessUtils procUtils;
     private SdkInit sdk;
 
-    String elfUrl = "http://151.145.35.129:8000/elfldr.elf";
+    String elfUrl = "http://172.245.146.114:8000/elfldr.elf";
 
     private boolean init() {
         try {
